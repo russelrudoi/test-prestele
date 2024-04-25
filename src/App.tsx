239df58a3e43661b14d99447449d4b3a70594e58
Ruslan Selezen <selezen.ua30@gmail.com>
2024-04-25 16:05:37 +0300

    [+] add style for home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import Home from "./pages/home/Home.tsx";
+import { Route, Routes } from 'react-router-dom'
+import AdminPanel from '@pages/admin-panel/AdminPanel.tsx'
 import '@styles/global.scss'
+import Home from './pages/home/Home.tsx'
 
 function App() {
-  return (
-    <>
-      <Home/>
-    </>
-  )
+	return (
+		<Routes>
+			<Route path={'/'} element={<Home />} />
+			<Route path={'/admin-panel'} element={<AdminPanel />} />
+		</Routes>
+	)
 }
 
 export default App
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,42 +1,45 @@
-import s from './style.module.scss'
+import clsx from 'clsx'
+import { Link } from 'react-router-dom'
+import { Button } from '@components/button/button.tsx'
 import ArrowSvg from '@assets/icon/arrow.svg?react'
-import {Button} from "@components/button/button.tsx";
-import clsx from "clsx";
+import s from './style.module.scss'
 
 const Home = () => {
-    return (
-        <section className={s.wrapper}>
-            <div className={clsx(s.adminPanel, s.container)}>
-                <p className={s.suptitle}>Test 1</p>
-                <h2 className={s.title}>
-                    Admin Panel<br/>
-                    Manage Category Page
-                </h2>
-                <Button icon={<ArrowSvg/>} className={s.btnNav}>
-                    Case Study
-                </Button>
-            </div>
-            <div className={clsx(s.hoverAnim, s.container)}>
-                <p className={s.suptitle}>Test 2</p>
-                <h2 className={s.title}>
-                    Hover animation<br/>
-                    With drag and drop
-                </h2>
-                <Button icon={<ArrowSvg/>} className={s.btnNav}>
-                    Case Study
-                </Button>
-            </div>
-            <div className={clsx(s.graphicAnim, s.container)}>
-                <p className={s.suptitle}>Test 3</p>
-                <h2 className={s.title}>
-                    Graphic animation
-                </h2>
-                <Button icon={<ArrowSvg/>} className={s.btnNav}>
-                    Case Study
-                </Button>
-            </div>
-        </section>
-    );
-};
+	return (
+		<section className={s.wrapper}>
+			<div className={clsx(s.adminPanel, s.container)}>
+				<div className={s.adminPanelInfo}>
+					<p className={s.suptitle}>Test 1</p>
+					<h2 className={s.title}>
+						Admin Panel <br />
+						Manage Category Page
+					</h2>
+					<Link to={'/admin-panel'}>
+						<Button icon={<ArrowSvg />} className={s.btnNav}>
+							Case Study
+						</Button>
+					</Link>
+				</div>
+			</div>
+			<div className={clsx(s.hoverAnim, s.container)}>
+				<p className={s.suptitle}>Test 2</p>
+				<h2 className={s.title}>
+					Hover animation <br />
+					With drag and drop
+				</h2>
+				<Button icon={<ArrowSvg />} className={s.btnNav}>
+					Case Study
+				</Button>
+			</div>
+			<div className={clsx(s.graphicAnim, s.container)}>
+				<p className={s.suptitle}>Test 3</p>
+				<h2 className={s.title}>Graphic animation</h2>
+				<Button icon={<ArrowSvg />} className={s.btnNav}>
+					Case Study
+				</Button>
+			</div>
+		</section>
+	)
+}
 
-export default Home;
+export default Home
